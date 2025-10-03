9e6164c9646283eb6b2635917479bebc566cb0f9
Omar Elmasri <omarmasri@duck.com>
2025-10-03 18:46:27 +0300

    feat:ch4-l2 , block types - headings only.

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,5 @@
 import re
+from enum import Enum
 
 
 def markdown_to_blocks(text):
@@ -6,3 +7,33 @@ def markdown_to_blocks(text):
     splits = re.split(pattern, text)
     return [s.strip() for s in splits]
     pass
+
+
+class BlockType(Enum):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered_list"
+    ordered_list = "ordered_list"
+
+
+def block_to_block_type(block: str):
+    """
+    Takes a single block of markdown text as input and returns the `BlockType` representing the type of block it is.
+
+    This will compare block to multiple regex.
+    """
+    ret = BlockType.paragraph
+
+    pattern_type = {
+        r"#{1,6}\s+": BlockType.heading,
+    }
+
+    for pat, type in pattern_type.items():
+        match = re.match(pat, block)
+        if match:
+            ret = type
+            break
+
+    return ret
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -4,6 +4,36 @@ import blocks as bk
 
 
 class Testutils(unittest.TestCase):
+    def test_block_type_heading1(self):
+        md = "# Heading1"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
+    def test_block_type_heading2(self):
+        md = "## Heading2"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
+    def test_block_type_heading3(self):
+        md = "### Heading3"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
+    def test_block_type_heading4(self):
+        md = "#### Heading4"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
+    def test_block_type_heading5(self):
+        md = "##### Heading5"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
+    def test_block_type_heading6(self):
+        md = "###### Heading6"
+        type = bk.block_to_block_type(md)
+        self.assertEqual(type, bk.BlockType.heading)
+
     def test_markdown_to_blocks(self):
         md = """
 This is **bolded** paragraph
