ffc0c616997366c1c21c77411a23d89b52d89c92
Omar Elmasri <omarmasri@duck.com>
2025-11-08 07:10:43 +0200

    fix: filter out empty blocks.

--- src/blocks.py
+++ src/blocks.py
@@ -7,8 +7,12 @@ import src.htmlnode as hn
 def markdown_to_blocks(text):
     pattern = r"\n{2,}"
     splits = re.split(pattern, text)
-    return [s.strip() for s in splits]
-    pass
+    ret = []
+    for split in splits:
+        s = split.strip()
+        if s:
+            ret.append(s)
+    return ret
 
 
 class BlockType(Enum):
