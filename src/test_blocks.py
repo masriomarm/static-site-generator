bf9fa5163a9a0bea0514664ab492a02d5d55e72f
Omar Elmasri <omarmasri@duck.com>
2025-11-08 07:11:13 +0200

    test: add test_paragraphs, makrdown paragraph to html.

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -4,6 +4,23 @@ import src.blocks as bk
 
 
 class Testutils(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = bk.markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
     def test_block_type_list_ordered(self):
         md = """
         1. ordered list text 1
