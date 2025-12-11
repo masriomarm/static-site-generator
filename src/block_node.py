4b877bf2134be1af5274a4fe65f5bc64200888c9
Omar Elmasri <omarmasri@duck.com>
2025-12-12 01:07:20 +0200

    feat(BlockNode): add `class BlockNodeParagraph(BlockNode)`.

--- src/block_node.py
+++ src/block_node.py
@@ -1,5 +1,7 @@
 from enum import Enum
 import src.blocks as bk
+import src.utils as ut
+import src.htmlnode as hn
 
 
 class BlockType(Enum):
@@ -22,20 +24,39 @@ class BlockNode:
         self.type: BlockType = bk.block_to_block_type(val)
         self.tag = self.type.value
         self.children = []
+        self.textNodes = []
 
     def __repr__(self):
         ret = f"""
         BlockNode(
-        Type: {self.type}
-        Tag: {self.tag}
-        Value: {self.value}
-        Children: {self.children}
+        Type: `{self.type}`
+        Tag: `{self.tag}`
+        Value: `{self.val}`
+        TextNodes: `{self.textNodes}`
+        Children: `{self.children}`
         )
         """
         return ret
 
+    def to_html():
+        raise NotImplementedError(
+            "Each block type will override this method to render themselves as HTML"
+        )
 
-# class BlockNodeParagraph(BlockNode):
+
+class BlockNodeParagraph(BlockNode):
+    def __init__(self, val):
+        super().__init__(val)
+        self.textNodes = ut.text_to_textnodes(self.val)
+        for node in self.textNodes:
+            html_node = ut.text_node_to_html_node(node)
+            leaf_node = hn.LeafNode(html_node.tag, html_node.value, html_node.props)
+            self.children.append(leaf_node)
+
+    def to_html(self):
+        return hn.ParentNode(self.tag, self.children).to_html()
+
+
 # class BlockNodeQuote(BlockNode):
 # class BlockNodeCode(BlockNode):
 # class BlockNodeHeading(BlockNode):
