af3bb02c3b4d5cf6f91a0934f5d706b85f42aaf5
Omar Elmasri <omarmasri@duck.com>
2026-01-07 17:15:15 +0200

    feat: working generation and serving

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
+import shutil
 import helper_utils as hu
+from pathlib import Path
 
 
 def main():
-    hu.util_copy_tree("static", "public", debug=False)
+    curPath = Path(__file__)
+    curDir = curPath.parent
+    trgPath = Path.joinpath(curDir.parent, "public")  # target path
+    srcPath = Path.joinpath(curDir.parent, "static")  # source path
+    try:
+        shutil.rmtree(trgPath)
+    except FileNotFoundError:
+        pass
+    hu.util_copy_tree(srcPath, trgPath, debug=False)
+
+    srcPath = Path.joinpath(curDir.parent, "content", "index.md")  # source path
+    trgPath = Path.joinpath(curDir.parent, "public", "index.html")  # content path
+    tmpPath = Path.joinpath(curDir.parent, "template.html")  # content path
+    hu.generate_page(srcPath, tmpPath, trgPath)
 
 
 main()
